6527736f42ffcc8cc39f2c4c9c6cd11305ece66a
Mido Abu-Akeel <akeelmido@gmail.com>
2023-08-24 20:55:06 -0400

    adding test for user options

--- src/pip/_internal/cli/base_command.py
+++ src/pip/_internal/cli/base_command.py
@@ -225,7 +225,7 @@ class Command(CommandContextMixIn):
 
             return exc_logging_wrapper
 
-        cmdoptions.validate_platform_options(options)
+        cmdoptions.validate_implementation_options(options)
 
         try:
             if not options.debug_mode:
